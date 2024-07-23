36f2dcd43526fee155dcb8195fe14c40d4e01f64
David Cen Pool <cendavid919@gmail.com>
2024-07-22 19:41:40 -0600

    commit inicial

---
+++ lib/UI/widgets/bottom_nav_bar.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class NabInf extends StatefulWidget {
+  const NabInf({super.key});
+
+  @override
+  State<NabInf> createState() => _NabInfState();
+}
+
+class _NabInfState extends State<NabInf> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Eventos'),
+    Text('Compras'),
+    Text("Mapa"),
+    Text("Notificaciones"),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.trolley),
+            label: 'Compras',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Mapa',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notification_add),
+            label: 'Notificaciones',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.normal),
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Color.fromARGB(255, 58, 18, 74),
+      ),
+    );
+  }
+}
